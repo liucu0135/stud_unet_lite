98494f5be943599623295e7641f5a095515627bf
liucu0135 <darkcoke2005@163.com>
2020-11-21 01:39:00 +0800

    1

--- self_supervised/train.py
+++ self_supervised/train.py
@@ -75,7 +75,7 @@ for pretext_id in range(1,9):
     # md_train = myDataset_unlabel('./mat/' + name + '/stud_data_train.mat', aug=False, inch=3)
     md_test = myDataset(path_test, aug=False, inch=3)
     # md_test = myDataset_unlabel('./mat/' + name + '/stud_data_test.mat', aug=False, inch=3)
-    load=True
+    load=False
     net = SUNET(in_ch=3, out_ch=2, ss=False, train_ext=not load, ff=True)
     if load:
         load_path = './checkpoints/' + 'all' + '/self_sup/net_stack_ssda_mul-dom{}.path'.format(pretext_id)
@@ -89,7 +89,7 @@ for pretext_id in range(1,9):
     net=net.cuda()
         # net.load_net(load_path)
 
-    train_loader = torch.utils.data.DataLoader(md_train, batch_size=10, shuffle=True, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(md_train, batch_size=10, shuffle=False, num_workers=0)
     validation_loader = torch.utils.data.DataLoader(md_test, batch_size=1,num_workers=0)
 
     train_loss = []
