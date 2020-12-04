c1a5b1e16077a19e8c4c099aa9b4af289d25575b
liucu0135 <darkcoke2005@163.com>
2020-12-04 16:39:20 +0800

    ri_only

--- self_supervised/train_da_all.py
+++ self_supervised/train_da_all.py
@@ -12,7 +12,7 @@ from datasets.Stud_Data_alltype_aligned import myDataset
 
 # trainloader=Data.DataLoader(md,batch_size=16,shuffle=True, num_workers=12)
 torch.cuda.set_device(0)
-total_epochs = 501
+total_epochs = 2001
 print_inter = 10
 vali_inter = 10
 validation_split = 0.2
